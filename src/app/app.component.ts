15a6ee81afacbe799962a7d74c0aa1b1dbd6333a
YonePC <YonePC@YONE>
2018-01-20 13:53:16 +0000

    declared AMI variable and loaded library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var AMI:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
 }
