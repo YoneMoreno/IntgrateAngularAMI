d442be8c4e289a1292799e199b134210a048455f
YonePC <YonePC@YONE>
2018-01-21 09:47:28 +0000

    load image data OK, CSS do not show up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,96 +1,100 @@
-import { Component } from '@angular/core';
-declare var AMI:any;
+import {Component, OnInit} from '@angular/core';
+
+declare var AMI: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  /* globals AMI*/
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    /* globals AMI*/
 
 // element to contain the progress bar
-   container = document.getElementById('container');
+    var container;
+    container = document.getElementById('container');
 
 // instantiate the loader
-  loader = new AMI.VolumeLoader(this.container);
+    var loader;
+    loader = new AMI.VolumeLoader(container);
 
-  t2 = ['36444280', '36444294', '36444308', '36444322', '36444336'];
-  files = this.t2.map(function(v) {
-    return 'https://cdn.rawgit.com/FNNDSC/data/master/dicom/adi_brain/' + v;
-  });
+    var t2;
+    t2 = ['36444280', '36444294', '36444308', '36444322', '36444336'];
+    var files;
+    files = t2.map(function (v) {
+      return 'https://cdn.rawgit.com/FNNDSC/data/master/dicom/adi_brain/' + v;
+    });
+    loader
+      .load(files)
+      .then(function () {
+        // merge files into clean series/stack/frame structure
+        var series = loader.data[0].mergeSeries(loader.data);
+        loader.free();
+        loader = null;
 
+        // series/stacks/frames are ready to be used
+        window.console.log(series);
 
-
-// once all files have been loaded (fetch + parse + add to array)
-// merge them into series / stacks / frames
-   loader
-.load(this.files)
-.then(function() {
-    // merge files into clean series/stack/frame structure
-     var series = this.loader.data[0].mergeSeries(this.loader.data);
-    this.loader.free();
-    this.loader = null;
-
-    // series/stacks/frames are ready to be used
-    window.console.log(series);
-
-    // Display some content on the DOM
-    var seriesIndex = 1;
-    for (var mySeries of series) {
-      var seriesDiv = document.createElement('div');
-      seriesDiv.className += 'indent';
-      seriesDiv.insertAdjacentHTML('beforeend', '<div> SERIES (' + seriesIndex + '/' + series.length + ')</div>');
-      seriesDiv.insertAdjacentHTML(
-        'beforeend',
-        '<div class="series"> numberOfChannels: ' + mySeries.numberOfChannels + '</div>'
-      );
-
-      this.container.appendChild(seriesDiv);
-
-      // loop through stacks
-      var stackIndex = 1;
-      for (var myStack of mySeries.stack) {
-        var stackDiv = document.createElement('div');
-        stackDiv.className += 'indent';
-        stackDiv.insertAdjacentHTML(
-          'beforeend',
-          '<div> STACK (' + stackIndex + '/' + mySeries.stack.length + ')</div>'
-        );
-        stackDiv.insertAdjacentHTML(
-          'beforeend',
-          '<div class="stack"> bitsAllocated: ' + myStack.bitsAllocated + '</div>'
-        );
-
-        seriesDiv.appendChild(stackDiv);
-
-        // loop through frames
-        var frameIndex = 1;
-        for (var myFrame of myStack.frame) {
-          var frameDiv = document.createElement('div');
-          frameDiv.className += 'indent';
-          frameDiv.insertAdjacentHTML(
+        // Display some content on the DOM
+        var seriesIndex = 1;
+        for (var mySeries of series) {
+          var seriesDiv = document.createElement('div');
+          seriesDiv.className += 'indent';
+          seriesDiv.insertAdjacentHTML('beforeend', '<div> SERIES (' + seriesIndex + '/' + series.length + ')</div>');
+          seriesDiv.insertAdjacentHTML(
             'beforeend',
-            '<div> FRAME (' + frameIndex + '/' + myStack.frame.length + ')</div>'
-          );
-          frameDiv.insertAdjacentHTML(
-            'beforeend',
-            '<div class="frame"> instanceNumber: ' + myFrame.instanceNumber + '</div>'
+            '<div class="series"> numberOfChannels: ' + mySeries.numberOfChannels + '</div>'
           );
 
-          stackDiv.appendChild(frameDiv);
-          frameIndex++;
+          container.appendChild(seriesDiv);
+
+          // loop through stacks
+          var stackIndex = 1;
+          for (var myStack of mySeries.stack) {
+            var stackDiv = document.createElement('div');
+            stackDiv.className += 'indent';
+            stackDiv.insertAdjacentHTML(
+              'beforeend',
+              '<div> STACK (' + stackIndex + '/' + mySeries.stack.length + ')</div>'
+            );
+            stackDiv.insertAdjacentHTML(
+              'beforeend',
+              '<div class="stack"> bitsAllocated: ' + myStack.bitsAllocated + '</div>'
+            );
+
+            seriesDiv.appendChild(stackDiv);
+
+            // loop through frames
+            var frameIndex = 1;
+            for (var myFrame of myStack.frame) {
+              var frameDiv = document.createElement('div');
+              frameDiv.className += 'indent';
+              frameDiv.insertAdjacentHTML(
+                'beforeend',
+                '<div> FRAME (' + frameIndex + '/' + myStack.frame.length + ')</div>'
+              );
+              frameDiv.insertAdjacentHTML(
+                'beforeend',
+                '<div class="frame"> instanceNumber: ' + myFrame.instanceNumber + '</div>'
+              );
+
+              stackDiv.appendChild(frameDiv);
+              frameIndex++;
+            }
+
+            stackIndex++;
+          }
+
+          seriesIndex++;
         }
+      })
+      .catch(function (error) {
+        window.console.log('oops... something went wrong...');
+        window.console.log(error);
+      });
 
-        stackIndex++;
-      }
+  }
 
-      seriesIndex++;
-    }
-  })
-.catch(function(error) {
-    window.console.log('oops... something went wrong...');
-    window.console.log(error);
-  });
 
 }
